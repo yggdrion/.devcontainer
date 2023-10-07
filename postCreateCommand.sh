2b20418df7ddfdbfbbc5e7a71a9940af8ee8c32d
r4b2 <rapha@r4b2.de>
2023-10-07 12:19:05 +0000

    update devcontainer

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -6,7 +6,7 @@ SHELLCHECK_VERSION="latest"
 
 pip install --upgrade pip
 pip install -r requirements.txt
-pip install -r requirements-dev.txt
+pip install -r ./devcontainer/requirements.txt
 
 git config --global user.email "rapha@r4b2.de"
 git config --global user.name "r4b2"
